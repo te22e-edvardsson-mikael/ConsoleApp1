0476a2be9b75c6481deeeb6fe04ba3ba024496e4
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-08-31 10:26:54 +0200

    första commiten

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string name = Console.ReadLine();
+
+console.WriteLine("ja eller nej?");
+
+if (name == "ja")
+{
+    Console.WriteLine("bra då fortsätter vi");
+}
+else 
+{
+    Console.WriteLine("varför inte?");
+}
